b3aa4b5d3b2b6b30f22cad1dd660862866d875d7
agustinrosa22 <agustinrosa1234@gmail.com>
2024-02-21 21:51:18 -0300

    css and responsive landing

--- my-app/src/views/Landing/Landing.jsx
+++ my-app/src/views/Landing/Landing.jsx
@@ -1,32 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Landing.module.css';
+import title from '../../assets/Doc2Home-Sin-fondo.png'
 
 const Landing = () => {
   return (
-    <div className={styles.homeContainer}>
-      <h1>¡Conéctate con doctores desde casa!</h1>
-      <p>Nuestra aplicación te ofrece atención médica de calidad desde la comodidad de tu hogar. Con acceso instantáneo a doctores expertos las 24 horas del día, los 7 días de la semana, podrás recibir la atención que necesitas sin esperas ni desplazamientos. Descarga nuestra app ahora y cuida tu salud de manera fácil y conveniente.</p>
-      
-      <div className={styles.languageSelector}>
-        <select>
-          <option value="english">English</option>
-          <option value="spanish">Español</option>
-        </select>
-      </div>
+    <div className={styles.pageContainer}>
+      <div className={styles.homeContainer}>
+        <img className={styles.img} src={title} alt="" />
+        <div className={styles.card}>
+        <h1>¡Conéctate con doctores desde casa!</h1>
+        <p className={styles.compactParagraph}>
+            Nuestra aplicación te ofrece atención médica de calidad desde la comodidad de tu hogar. Con acceso instantáneo a doctores expertos las 24 horas del día, los 7 días de la semana, podrás recibir la atención que necesitas sin esperas ni desplazamientos. Descarga nuestra app ahora y cuida tu salud de manera fácil y conveniente.
+          </p>
+          
+          <div className={styles.selectorsContainer}>
+            <div className={styles.languageSelector}>
+              <select>
+                <option value="english">English</option>
+                <option value="spanish">Español</option>
+              </select>
+            </div>
+            <div className={styles.currencySelector}>
+              <select>
+                <option value="USD">USD</option>
+                <option value="ARG">ARG</option>
+              </select>
+            </div>
+          </div>
 
-      <div className={styles.currencySelector}>
-        <select>
-          <option value="USD">USD</option>
-          <option value="ARG">ARG</option>
-        </select>
+          <div className={styles.buttonContainer}>
+            <Link to="/home" className={styles.loginButton}>Login</Link>
+            <Link to="/register" className={styles.registerButton}>Register</Link>
+          </div>
+        </div>
+        </div>
       </div>
-
-      <div className={styles.buttonContainer}>
-        <Link to="/home" className={styles.loginButton}>Login</Link>
-        <Link to="/register" className={styles.registerButton}>Register</Link>
-      </div>
-    </div>
   );
 }
 
